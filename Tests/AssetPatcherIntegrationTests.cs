3be4f4ec0a68f9ae9eef2b2b4d052d9acc0aef75
3ter <drinc3@gmail.com>
2025-05-15 16:14:35 +0200

    Add test files with specific path ids

--- Tests/AssetPatcherIntegrationTests.cs
+++ Tests/AssetPatcherIntegrationTests.cs
@@ -5,7 +5,7 @@ using UnityAssetBundlePatcher.AssetPatcherLib;
 using AssetsTools.NET;
 using AssetsTools.NET.Extra;
 
-namespace UnityAssetBundlePatcher.Tests
+namespace Tests
 {
     [TestFixture]
     public class AssetPatcherIntegrationTests
@@ -14,46 +14,38 @@ namespace UnityAssetBundlePatcher.Tests
         public void PatchRawAsset_AppliesPatchFromCorrectlyNamedFile()
         {
             // Arrange: use embedded asset file and patch data
+            string assetFileTemplate = Path.Combine(TestContext.CurrentContext.TestDirectory, "resources.assets");
+            string patchFileTemplate = Path.Combine(TestContext.CurrentContext.TestDirectory, "Solid_001-resources.assets-6.dat");
+
             string testDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
             Directory.CreateDirectory(testDir);
-            string assetFile = Path.Combine(testDir, "test-asset.assets");
-            string patchFile = Path.Combine(testDir, "test-12345.dat");
-            string outputFile = Path.Combine(testDir, "output.assets");
+            string assetFile = Path.Combine(testDir, "test-resources.assets");
+            string outputFile = Path.Combine(testDir, "test-resources.assets.patch");
+            string patchFile = Path.Combine(TestContext.CurrentContext.TestDirectory, "Solid_001-resources.assets-5.dat");
 
             // Copy the real asset file from test resources
-            File.Copy(Path.Combine(TestContext.CurrentContext.TestDirectory, "test-asset.assets"), assetFile);
+            File.Copy(assetFileTemplate, assetFile);
+            // Copy the patch file from test resources and give it another id to replace
+            File.Copy(patchFileTemplate, patchFile);
 
-            // Write patch data (example: 4 bytes)
-            byte[] patchData = { 0xDE, 0xAD, 0xBE, 0xEF };
-            File.WriteAllBytes(patchFile, patchData);
+            // Read the mesh data from the file before replacing it
+            var manager = new AssetsManager();
+            var fileInst = manager.LoadAssetsFile(assetFile, false);
+            var afile = fileInst.file;
+            var assetInfo1 = afile.GetAssetsOfType(AssetClassID.Mesh).Find(a => a.PathId == 5);
+            var assetInfo2 = afile.GetAssetsOfType(AssetClassID.Mesh).Find(a => a.PathId == 6);
+
+            Assert.That(!assetInfo1.ByteSize.Equals(assetInfo2.ByteSize), "Before the patch the meshes should not have the same size");
 
             // Act
             AssetPatcher.PatchRawAsset(assetFile, patchFile, outputFile);
 
             // Assert: output file should contain the patched data at the correct asset
-            var manager = new AssetsManager();
-            var fileInst = manager.LoadAssetsFile(outputFile, false);
-            var afile = fileInst.file;
-            // Find the asset with pathId 12345
-            var assetInfo = afile.GetAssetInfo(12345);
-            // Read the raw bytes from the asset's data using BaseField
-            var baseField = manager.GetBaseField(fileInst, assetInfo);
-            byte[] data = null;
-            if (baseField != null)
-            {
-                // Try common field names for raw byte data
-                var scriptField = baseField["m_Script"];
-                var bytesField = baseField["m_Bytes"];
-                if (scriptField != null && scriptField.Value != null && scriptField.Value.ValueType == AssetValueType.ByteArray)
-                {
-                    data = scriptField.Value.AsByteArray;
-                }
-                else if (bytesField != null && bytesField.Value != null && bytesField.Value.ValueType == AssetValueType.ByteArray)
-                {
-                    data = bytesField.Value.AsByteArray;
-                }
-            }
-            Assert.That(data, Is.EqualTo(patchData), "Patched data does not match expected bytes.");
+            var fileInstNew = manager.LoadAssetsFile(outputFile, false);
+            var afileNew = fileInst.file;
+            var assetInfoNew1 = afile.GetAssetsOfType(AssetClassID.Mesh).Find(a => a.PathId == 6);
+            var assetInfoNew2 = afile.GetAssetsOfType(AssetClassID.Mesh).Find(a => a.PathId == 6);
+            Assert.That(assetInfoNew1.ByteSize.Equals(assetInfoNew2.ByteSize), "After the patch both meshes should have the same size");
         }
     }
 }
